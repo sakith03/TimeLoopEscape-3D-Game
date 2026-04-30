d389e77e00174a6edebe6907437360c075e58d74
chathura2406 <it23689480@my.sliit.lk>
2026-04-30 10:52:36 +0530

    Added Door logics final

--- UnityAssets/3D TimeLoopEscape/Assets/Character/PlayerMovement.cs
+++ UnityAssets/3D TimeLoopEscape/Assets/Character/PlayerMovement.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    public CharacterController controller; 
+    
     public float moveSpeed = 3.5f;
     public float rotationSpeed = 250f;
     public Animator animator;
@@ -11,12 +14,17 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxis("Vertical");   // W/S
         float h = Input.GetAxis("Horizontal"); // A/D
 
-        // ROTATE CHARACTER (A/D ONLY)
+        
         transform.Rotate(0, h * rotationSpeed * Time.deltaTime, 0);
 
-        // MOVE FORWARD/BACKWARD
-        Vector3 move = transform.forward * v;
-        transform.position += move * moveSpeed * Time.deltaTime;
+        
+        Vector3 moveDirection = transform.forward * v;
+
+        
+        if (controller != null)
+        {
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        }
 
         // ANIMATION
         animator.SetBool("isWalking", Mathf.Abs(v) > 0.1f);
